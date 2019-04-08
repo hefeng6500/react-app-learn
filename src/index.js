55174a1f02fb54a8e5f4846394a1d5463b786021
hefeng6500 <2443992009@qq.com>
2019-04-08 21:36:51 +0800

    修改了css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import "./App.css";
+import "./index.css";
 import { Provider } from 'react-redux'
 import store from './store'
 import TodoList from "./todolist";
